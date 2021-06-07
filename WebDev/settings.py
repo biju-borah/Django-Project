a2913ecd01b638b04ffdb90a532c0dfff353f74c
Biju Borah <82822220+biju-borah@users.noreply.github.com>
2021-06-07 14:46:18 +0530

    Add files via upload

--- WebDev/settings.py
+++ WebDev/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 #Added manually
